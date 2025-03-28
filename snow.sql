ee790d8b3cd6c0358f813c7e328339bdaad6ea61
AyeshaKazi <ayeshakazi1612@gmail.com>
2025-03-28 10:10:02 -0400

    Update snow.sql

--- snow.sql
+++ snow.sql
@@ -33,3 +33,63 @@ ORDER BY bkgs.book_ref, tkts.ticket_no, tkt_flts.flight_id, flts.aircraft_code
 ;
 
 SELECT * FROM aw_spcs_db.public.all_flight_details;
+
+
+
+-- Setting context
+USE ROLE accountadmin;
+USE WAREHOUSE compute_wh;
+USE DATABASE aw_spcs_db;
+USE SCHEMA aw_spcs_db.public;
+
+-- Cortex Search
+-- Create a preprocessing function to parse the PDF file, extract the text, and chunk the text into smaller pieces for indexing.
+CREATE OR REPLACE FUNCTION aw_spcs_db.public.pdf_text_chunker(file_url STRING)
+    RETURNS TABLE (chunk VARCHAR)
+    LANGUAGE PYTHON
+    RUNTIME_VERSION = '3.9'
+    HANDLER = 'pdf_text_chunker'
+    PACKAGES = ('snowflake-snowpark-python', 'PyPDF2', 'langchain')
+    AS
+$$
+from snowflake.snowpark.types import StringType, StructField, StructType
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from snowflake.snowpark.files import SnowflakeFile
+import PyPDF2, io
+import logging
+import pandas as pd
+
+class pdf_text_chunker:
+
+    def read_pdf(self, file_url: str) -> str:
+        logger = logging.getLogger("udf_logger")
+        logger.info(f"Opening file {file_url}")
+
+        with SnowflakeFile.open(file_url, 'rb') as f:
+            buffer = io.BytesIO(f.readall())
+
+        reader = PyPDF2.PdfReader(buffer)
+        text = ""
+        for page in reader.pages:
+            try:
+                text += page.extract_text().replace('\n', ' ').replace('\0', ' ')
+            except:
+                text = "Unable to Extract"
+                logger.warn(f"Unable to extract from file {file_url}, page {page}")
+
+        return text
+
+    def process(self, file_url: str):
+        text = self.read_pdf(file_url)
+
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size = 2000,  # Adjust this as needed
+            chunk_overlap = 300,  # Overlap to keep chunks contextual
+            length_function = len
+        )
+
+        chunks = text_splitter.split_text(text)
+        df = pd.DataFrame(chunks, columns=['chunk'])
+
+        yield from df.itertuples(index=False, name=None)
+$$;
